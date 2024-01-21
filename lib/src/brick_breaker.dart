0f795ba61c432a1dac8a9945fb1281470c88a92a
Damilola Adeniyi <adeniiyidamilola246@gmail.com>
2024-01-21 19:09:07 +0100

    add ball collision to the play area walls

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -4,8 +4,9 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'components/components.dart';
 import 'config.dart';
+import 'dart:math' as math;
 
-class BrickBreaker extends FlameGame {
+class BrickBreaker extends FlameGame with HasCollisionDetection {
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -16,7 +17,7 @@ class BrickBreaker extends FlameGame {
 
   double get width => size.x;
   double get height => size.y;
-
+  final rand = math.Random();
   @override
   FutureOr<void> onLoad() async {
     super.onLoad();
@@ -24,5 +25,11 @@ class BrickBreaker extends FlameGame {
     camera.viewfinder.anchor = Anchor.topLeft;
 
     world.add(PlayArea());
+    world.add(Ball(
+        radius: ballRadius,
+        position: size / 2,
+        velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2).normalized()..scale(height / 4)));
+
+    debugMode = true;
   }
 }
